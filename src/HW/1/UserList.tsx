6a1f7f6004dbb6287818b2568c13ec485403aef6
ivanrussui <fenomen_ny@mail.ru>
2024-01-24 19:16:28 +0300

    HW1

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,36 @@
+import {FC} from 'react';
+
+type AddressType = {
+    street: string;
+    city: string;
+};
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+    address: AddressType
+};
+
+type UserListPropsType = {
+    users: Array<UserType>;
+};
+
+export const UserList: FC<UserListPropsType> = ({users}) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {users.map(user => {
+                    return (
+                        <li key={user.id} id={`hw01-user-${user.id}`}>
+                            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                            {user.address.street}, {user.address.city}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
